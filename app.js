307c74e228616780c5cbd41a8fb856388bc3239d
Sampsa Kuronen <sampsa.kuronen@reaktor.com>
2018-05-23 11:49:50 +0300

    Revert "Add support for city bikes in Turku"

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const compress = require('compression')
 const Lokka = require('lokka').Lokka
 const Transport = require('lokka-transport-http').Transport
 const helmet = require('helmet')
-const fetch = require('node-fetch')
 
-let stationCaches = {}
+var stationCache = null
 
 const app = express()
 const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-const FOLI_REST_URL = 'http://data.foli.fi/citybike'
 const graphQLClient = new Lokka({
   transport: new Transport(HSL_GRAPHQL_URL)
 })
@@ -21,31 +19,10 @@ app.use(express.static('./public', {maxAge: 30 * 60 * 1000}))
 
 app.get('/api/stations', (req, res) => {
   res.setHeader('Cache-Control', 'public, max-age=5')
-  res.json({
-    bikeRentalStations: _.flatMap(Object.values(stationCaches), cache => cache.bikeRentalStations)
-  })
+  res.json(stationCache)
 })
 
-function refreshStationCacheFoli() {
-  fetch(FOLI_REST_URL)
-    .then(res => res.json())
-    .then(data => Object.values(data.racks))
-    .then(racks => ({
-        bikeRentalStations: racks.map(rack => ({
-          id: rack.id,
-          name: rack.name,
-          lat: rack.lat,
-          lon: rack.lon,
-          bikesAvailable: rack.bikes_avail,
-          spacesAvailable: rack.slots_avail
-        }))
-      })
-    ).then(result => {
-      stationCaches.foli = result
-    })
-}
-
-function refreshStationCacheHSL() {
+function refreshStationCache() {
   graphQLClient.query(`
     {
       bikeRentalStations {
@@ -58,20 +35,14 @@ function refreshStationCacheHSL() {
       }
     }
   `).then(result => {
-    stationCaches.hsl = result
+    stationCache = result
   })
-}
-
-function refreshStationCaches() {
-  // Update caches independently in order to isolate possible API hickups.
-  refreshStationCacheFoli()
-  refreshStationCacheHSL()
 }
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Kaupunkifillarit.fi listening on *:${port}`)
-  setInterval(refreshStationCaches, 5 * 1000)
-  refreshStationCaches()
+  setInterval(refreshStationCache, 5 * 1000)
+  refreshStationCache()
 })
 
